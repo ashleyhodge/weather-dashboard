54e1ec778c488dfac3293bbfbeef45545250114b
Ashley Hodge <ashhodge@comcast.net>
2022-01-25 20:22:04 -0500

    retreive data from api when a city name is submited

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
 var apiKey = "95ce3e162e0116e8f93a9e96b347dad9"
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city");
 
-var getWeatherUpdate = function(lat,lon) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid="+ apiKey;
+
+var getWeatherUpdate = function(cityName) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid="+ apiKey;
    fetch(apiUrl).then(function(response){
         response.json().then(function(data) {
             console.log(data)
         })
     });
 }
-getWeatherUpdate(63,43);
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var city = cityInputEl.value.trim();
+
+    if(city) {
+        getWeatherUpdate(city);
+        cityInputEl.value = "";
+    }
+};
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
